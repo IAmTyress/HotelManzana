ce4ffc7e759206e6f375b818a5d65dbd5d56fd19
IAmTyReSS <malyshtim2000@yandex.ru>
2021-08-13 12:37:20 +0300

    Add project Hotel Manzana

---
+++ HotelManzana/HotelManzana/SelectRoomTypeTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SelectRoomTypeTableViewController.swift
+//  HotelManzana
+//
+//  Created by Russ Rosaura on 7/18/21.
+//  Copyright © 2021 Russ Rosaura. All rights reserved.
+//
+
+import UIKit
+
+class SelectRoomTypeTableViewController: UITableViewController {
+    
+}
